8148575173c948e9175d216dca989b0cf210d96e
TEAM-PIKACHU-INDIA <73126271+TEAM-PIKACHU-INDIA@users.noreply.github.com>
2020-10-21 09:58:15 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,12 +1,5 @@
-from userbot import catdef
+from userbot import darkdef
 
-deEmojify = catdef.deEmojify
-trumptweet = catdef.trumptweet 
-changemymind = catdef.changemymind
-kannagen = catdef.kannagen
-moditweet = catdef.moditweet
-tweets = catdef.tweets
-waifutxt = catdef.waifutxt
-catmusic = catdef.catmusic 
-catmusicvideo = catdef.catmusicvideo
-admin_groups = catdef.admin_groups
+
+darkmusic = darkdef.darkmusic 
+darkmusicvideo = darkdef.darkmusicvideo
